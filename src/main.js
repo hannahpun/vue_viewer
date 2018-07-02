267af3f3e43d9dc55a792341dc3a8c06144379cb
hannah.lin@afusion.com <hannah.lin@afusion.com>
2018-07-02 18:12:42 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import underscore from 'vue-underscore'
+
+import store from './store.js'
+
+import './styles/element-variables.scss'
+import './styles/application.scss'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
+Vue.use(underscore)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
